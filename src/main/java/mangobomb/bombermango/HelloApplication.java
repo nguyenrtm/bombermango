6bc750ea1d8c3c0303e1160eb5eff09c39f95913
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2022-09-22 00:06:23 +0700

    add basic key inputs

--- src/main/java/mangobomb/bombermango/HelloApplication.java
+++ src/main/java/mangobomb/bombermango/HelloApplication.java
@@ -7,6 +7,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.UserAction;
+import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 import java.io.IOException;
 
@@ -15,13 +25,45 @@ public class HelloApplication extends GameApplication {
     protected void initSettings(GameSettings gameSettings) {
         gameSettings.setWidth(700);
         gameSettings.setHeight(700);
+        gameSettings.setTitle("Bomberman!");
     }
 
     @Override
     protected void initGame() {
         super.initGame();
         FXGL.getGameWorld().addEntityFactory(new GenerateFactory());
-        FXGL.getGameWorld().spawn("Rocket");
+    }
+
+    @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("Up 1") {
+            @Override
+            protected void onAction() {
+                System.out.println("Up");
+                FXGL.getGameWorld().spawn("Rocket");
+            }
+        }, KeyCode.W);
+
+        getInput().addAction(new UserAction("Left 1") {
+            @Override
+            protected void onAction() {
+                System.out.println("Left");
+            }
+        }, KeyCode.A);
+
+        getInput().addAction(new UserAction("Down 1") {
+            @Override
+            protected void onAction() {
+                System.out.println("Down");
+            }
+        }, KeyCode.S);
+
+        getInput().addAction(new UserAction("Right 1") {
+            @Override
+            protected void onAction() {
+                System.out.println("Right");
+            }
+        }, KeyCode.D);
     }
 
     public static void main(String[] args) {
